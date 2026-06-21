1183391bb8c04ab78076522406acce67cef6a7f4
bfrcode <bastien.frontoni@gmail.com>
2026-06-21 15:49:18 +0200

    work on speciality class

--- models/speciality.py
+++ models/speciality.py
@@ -1,7 +1,20 @@
 import sqlite3
 
+from dataclasses import dataclass
+
 from utils.string import normalize_speciality
 
+@dataclass
+class Speciality:
+    speciality_id: int
+    speciality_name: str
+
+    @classmethod
+    def from_row (cls, row: sqlite3.Row) -> "Speciality":
+        return cls(
+            speciality_id=row["speciality_id"],
+            speciality_name=row["speciality_type"],
+        )
 def get_or_create_speciality(conn: sqlite3.Connection, name: str) -> int:
     """Retourne l'id d'une spécialité ou la crée si elle n'existe pas"""
     speciality_name = normalize_speciality(name)
